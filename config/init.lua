43ea3127a9f147baa23785d3d092c74c3ede4575
Wei Cheng <xsnaruto@users.noreply.github.com>
2024-12-20 17:39:23 +0800

    Update init.lua

--- config/init.lua
+++ config/init.lua
@@ -1,109 +1,202 @@
--- Basic settings
-vim.opt.encoding = 'utf-8'                -- 设置编码为 UTF-8
-vim.opt.termencoding = 'utf-8'            -- 设置终端编码为 UTF-8
-vim.opt.fileencoding = 'utf-8'           -- 设置文件编码为 UTF-8
+-- Init.lua
+--  __  __        __     _____ __  __    ____             __ _       
+-- |  \/  |_   _  \ \   / /_ _|  \/  |  / ___|___  _ __  / _(_) __ _ 
+-- | |\/| | | | |  \ \ / / | || |\/| | | |   / _ \| '_ \| |_| |/ _` |
+-- | |  | | |_| |   \ V /  | || |  | | | |__| (_) | | | |  _| | (_| |
+-- |_|  |_|\__, |    \_/  |___|_|  |_|  \____\___/|_| |_|_| |_|\__, |
+--         |___/                                               |___/ 
 
-vim.opt.list = true                       -- 显示制表符和空格
-vim.opt.listchars = { tab = '┆' }         -- 使用竖线表示 TAB 键
+-- 基础设置
+require('options')
+-- 快捷键映射
+require('keymaps')
+-- 插件管理
+require('plugins')
+-- 插件配置
+require('plugin-config/coc')
+require('plugin-config/nerdtree')
 
-vim.cmd('syntax on')                      -- 启用语法高亮
+-- lua/options.lua
+local opt = vim.opt
 
-vim.opt.ignorecase = true                 -- 忽略搜索时的大小写
-vim.opt.smartcase = true                  -- 当搜索包含大写字母时，区分大小写
+-- 编码设置
+opt.encoding = 'utf-8'
+opt.fileencoding = 'utf-8'
 
-vim.opt.conceallevel = 0                 -- 禁止字符自动隐藏，如双引号
+-- 显示设置
+opt.list = true
+opt.listchars = { tab = '┆ ' }
+opt.syntax = 'on'
+opt.background = 'dark'
+opt.termguicolors = true
+
+-- 搜索设置
+opt.ignorecase = true
+opt.smartcase = true
 
 -- 缩进设置
-vim.opt.autoindent = false                -- 关闭自动缩进
-vim.opt.smartindent = true                -- 启用智能缩进
-vim.opt.tabstop = 4                      -- 一个 TAB 键相当于 4 个空格
-vim.opt.softtabstop = 4                  -- 插入 TAB 时缩进的宽度
-vim.opt.shiftwidth = 4                   -- 向后移动时的缩进宽度
-vim.opt.expandtab = true                 -- 使用空格代替 TAB
+opt.autoindent = false
+opt.smartindent = true
+opt.tabstop = 4
+opt.softtabstop = 4
+opt.shiftwidth = 4
+opt.expandtab = true
 
--- 行号设置
-vim.opt.number = true                     -- 显示行号
-vim.opt.relativenumber = false           -- 关闭相对行号
-vim.opt.cursorline = true                -- 高亮当前行
+-- 显示设置
+opt.number = true
+opt.relativenumber = false
+opt.cursorline = true
+opt.scrolloff = 7
+opt.conceallevel = 0
 
-vim.opt.scrolloff = 7                    -- 设置滚动时的空余行数
+-- 备份文件设置
+-- opt.backup = false
+-- opt.writebackup = false
+-- opt.swapfile = false
 
--- Backup/Swap File settings
-vim.cmd([[
-augroup AutoDeleteSwapOnWrite
-  autocmd!
-  -- 在执行保存命令后删除 swap 文件
-  autocmd BufWritePost * if exists("v:swapname") | call delete(v:swapname) | let v:swapname = '' | endif
-augroup END
-]])
+-- 自动删除swap文件
+vim.api.nvim_create_autocmd("BufWritePost", {
+    pattern = "*",
+    callback = function()
+        if vim.v.swapname ~= '' then
+            os.remove(vim.v.swapname)
+            vim.v.swapname = ''
+        end
+    end
+})
 
 -- 配色设置
-vim.opt.background = 'dark'              -- 设置背景为深色
-vim.opt.termguicolors = true             -- 支持更丰富的 256 色显示
-vim.cmd('colo molokai')                 -- 设置配色方案为 molokai
-vim.g.molokai_original = 1               -- 保持原始 molokai 配色
-vim.g.rehash256 = 1                     -- 强制重新加载 256 色
+vim.cmd [[
+    set t_Co=256
+    colorscheme molokai
+    let g:molokai_original = 1
+    let g:rehash256 = 1
+]]
 
--- Hotkey settings
-vim.g.mapleader = ' '                    -- 设置 Leader 键为空格
-vim.api.nvim_set_keymap('n', '<Leader>init', ':e ~/.config/nvim/init.vim<CR>', { noremap = true })
-vim.api.nvim_set_keymap('n', '<Leader>ft', ':set filetype=', { noremap = true })
-vim.api.nvim_set_keymap('n', '<C-r>', ':source %<CR>', { noremap = true })
-vim.api.nvim_set_keymap('n', 'K', '5k', { noremap = true })    -- 快速上滚 5 行
-vim.api.nvim_set_keymap('n', 'J', '5j', { noremap = true })    -- 快速下滚 5 行
-vim.api.nvim_set_keymap('n', 'W', ':w<CR>', { noremap = true }) -- 快速保存
-vim.api.nvim_set_keymap('n', 'Q', ':q<CR>', { noremap = true }) -- 快速退出
-vim.api.nvim_set_keymap('n', '<Leader><CR>', ':nohlsearch<CR>', { noremap = true }) -- 取消搜索高亮
-vim.api.nvim_set_keymap('v', 'Y', '"+y', { noremap = true })   -- 复制到系统剪切板
-vim.api.nvim_set_keymap('n', '<Leader>sg', ':%s//g<left><left>', { noremap = true }) -- 快速替换
+-- lua/keymaps.lua
+local keymap = vim.keymap.set
+local opts = { noremap = true, silent = true }
 
--- Windows manage
-vim.api.nvim_set_keymap('n', 'sj', ':split<CR>', { noremap = true })    -- 水平分割窗口
-vim.api.nvim_set_keymap('n', 'sl', ':vsplit<CR>', { noremap = true })   -- 垂直分割窗口
+-- 设置leader键
+vim.g.mapleader = " "
 
--- Tag manage
-vim.api.nvim_set_keymap('n', 'tt', ':tabe<CR>', { noremap = true })  -- 新增标签页
-vim.api.nvim_set_keymap('n', 'th', ':-tabnext<CR>', { noremap = true }) -- 关闭左侧标签页
-vim.api.nvim_set_keymap('n', 'tl', ':+tabnext<CR>', { noremap = true }) -- 关闭右侧标签页
+-- 快速打开配置文件
+keymap('n', 'init', ':e ~/.config/nvim/init.lua<CR>', opts)
 
--- Plugins manage
-vim.cmd([[
-if empty(glob('~/.config/nvim/autoload/plug.vim'))
-  :exe '!curl -fLo ~/.config/nvim/autoload/plug.vim --create-dirs
-          \ https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
-  au VimEnter * PlugInstall --sync | source $MYVIMRC
-endif
+-- 设置文件类型
+keymap('n', 'ft', ':set filetype=', opts)
 
-call plug#begin('~/.config/nvim/plugged')
-Plug 'vim-airline/vim-airline'                -- Vim 状态栏插件
-Plug 'nvim-treesitter/nvim-treesitter', { 'do': ':TSUpdate' } -- 语法高亮增强
-Plug 'neoclide/coc.nvim', { 'branch': 'release' } -- 补全插件
-Plug 'dstein64/nvim-scrollview', { 'branch': 'main' } -- 左侧滚动查看
-Plug 'mbbill/undotree'                        -- Undo 代码历史
-Plug 'Yggdroot/indentLine'                    -- 可视化缩进线
-Plug 'preservim/nerdtree', { 'tag': '7.1.2' } -- 文件树
-Plug 'jlanzarotta/bufexplorer', { 'commit': '20f0440' } -- 文件历史管理
-Plug 'github/copilot.vim'                     -- GitHub Copilot
-call plug#end()
-]])
+-- 重新加载配置
+keymap('n', '<C-r>', ':source %<CR>', opts)
 
--- Coc.vim settings
-vim.opt.updatetime = 100                     -- 设置自动更新时间间隔为 100ms
-vim.g.coc_global_extensions = {              -- 默认全局扩展
-  'coc-marketplace',
-  'coc-explorer',
-  'coc-sh',
-  'coc-html',
-  'coc-css',
-  'coc-tsserver',
-  'coc-python',
-  'coc-json',
-  'coc-yaml',
-  'coc-prettier'
+-- 快速滚动
+keymap('n', 'K', '5k', opts)
+keymap('n', 'J', '5j', opts)
+
+-- 保存和退出
+keymap('n', 'W', ':w<CR>', opts)
+keymap('n', 'Q', ':q<CR>', opts)
+keymap('n', 'FQ', ':q!<CR>', opts)
+keymap('n', 'S', ':w ', opts)
+
+-- 系统剪贴板
+keymap('v', 'Y', '"+y', opts)
+
+-- 取消搜索高亮
+keymap('n', '<leader><CR>', ':nohlsearch<CR>', opts)
+
+-- 查找替换
+keymap('n', 'sg', ':%s//g<left><left>', opts)
+
+-- 窗口管理
+keymap('n', 'sj', ':split<CR>', opts)
+keymap('n', 'sl', ':vsplit<CR>', opts)
+
+-- 标签页管理
+keymap('n', 'tt', ':tabe<CR>', opts)
+keymap('n', 'th', ':-tabnext<CR>', opts)
+keymap('n', 'tl', ':+tabnext<CR>', opts)
+
+-- lua/plugins.lua
+local fn = vim.fn
+
+-- 自动安装 packer
+local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+if fn.empty(fn.glob(install_path)) > 0 then
+  PACKER_BOOTSTRAP = fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+end
+
+-- 插件安装
+return require('packer').startup(function(use)
+    use 'wbthomason/packer.nvim'
+    -- use 'hardcoreplayers/dashboard-nvim'
+    use 'vim-airline/vim-airline'
+    use {
+        'nvim-treesitter/nvim-treesitter',
+        run = ':TSUpdate'
+    }
+    use {
+        'neoclide/coc.nvim',
+        branch = 'release'
+    }
+    -- use 'ervandew/supertab'
+    -- use {
+    --     'prettier/vim-prettier',
+    --     run = 'npm install'
+    -- }
+    -- use 'vim-autoformat/vim-autoformat'
+    use {
+        'dstein64/nvim-scrollview',
+        branch = 'main'
+    }
+    use 'mbbill/undotree'
+    use 'Yggdroot/indentLine'
+    use {
+        'WolfgangMehner/bash-support',
+        commit = '99c746c'
+    }
+    use {
+        'preservim/nerdtree',
+        tag = '7.1.2'
+    }
+    use {
+        'jlanzarotta/bufexplorer',
+        commit = '20f0440'
+    }
+    use 'github/copilot.vim'
+
+    if PACKER_BOOTSTRAP then
+        require('packer').sync()
+    end
+end)
+
+-- lua/plugin-config/coc.lua
+-- Coc配置
+vim.g.coc_global_extensions = {
+    'coc-marketplace',
+    'coc-explorer',
+    'coc-sh',
+    'coc-html',
+    'coc-css',
+    'coc-tsserver',
+    'coc-python',
+    'coc-json',
+    'coc-yaml',
+    'coc-prettier'
 }
 
--- NerdTree manage
-vim.api.nvim_set_keymap('n', 'fs', ':NERDTreeToggle<CR>', { noremap = true }) -- 开/关 NERDTree
+vim.opt.updatetime = 100
 
--- Other settings
-vim.api.nvim_set_keymap('n', '<C-F>', ':Autoformat', { noremap = true }) -- 格式化文件
+-- SuperTab配置
+vim.g.SuperTabDefaultCompletionType = "<c-n>"
+
+-- lua/plugin-config/nerdtree.lua
+-- NERDTree配置
+vim.keymap.set('n', 'fs', ':NERDTreeToggle<CR>', { noremap = true, silent = true })
+
+-- 其他设置
+-- IndentLine配置
+-- vim.g.indentLine_defaultGroup = 'SpecialKey'
+
+-- Prettier配置
+keymap('n', '<C-F>', ':Autoformat', opts)
