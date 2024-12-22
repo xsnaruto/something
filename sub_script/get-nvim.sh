5e4462742a479dd3071ae343906133101b4867de
Wei Cheng <xsnaruto@users.noreply.github.com>
2024-12-23 04:52:34 +0800

    Update get-nvim.sh

--- sub_script/get-nvim.sh
+++ sub_script/get-nvim.sh
@@ -12,18 +12,18 @@ sudo npm install -g n && n lts >>/dev/null 2>&1
 
 # Clean may exist junk file
 rm -rf ~/.config/nvim >>/dev/null 2>&1
-rm -rf ~/.config/coc >>/dev/null 2>&1
+# rm -rf ~/.config/coc >>/dev/null 2>&1
 # Create neovim config path
 mkdir ~/.config/nvim -p >>/dev/null 2>&1
 mkdir ~/.config/nvim/plugged >>/dev/null 2>&1
 
 # Install neovim plugin
-sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
-       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim' >>/dev/null 2>&1
+# sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+#       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim' >>/dev/null 2>&1
 
 # Neovim color install
-mkdir ~/.config/nvim/colors >>/dev/null 2>&1
-wget -O ~/.config/nvim/colors/molokai.vim https://raw.githubusercontent.com/tomasr/molokai/master/colors/molokai.vim >>/dev/null 2>&1
+# mkdir ~/.config/nvim/colors >>/dev/null 2>&1
+# wget -O ~/.config/nvim/colors/molokai.vim https://raw.githubusercontent.com/tomasr/molokai/master/colors/molokai.vim >>/dev/null 2>&1
 
 # Neovim config restore
 wget -O ~/.config/nvim/init.lua https://github.com/xsnaruto/something/raw/main/config/init.lua >>/dev/null 2>&1
