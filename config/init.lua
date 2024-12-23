cf4913ca0ca13ea3426d31e219fddbbba79f93fa
Wei Cheng <xsnaruto@users.noreply.github.com>
2024-12-24 01:12:49 +0800

    Update init.lua

--- config/init.lua
+++ config/init.lua
@@ -127,11 +127,42 @@ vim.opt.rtp:prepend(lazypath)
 require("lazy").setup({
   -- coc.nvim 的插件配置
   {
-    'goolord/alpha-nvim',
-    dependencies = { 'echasnovski/mini.icons' },
-    config = function ()
-        require'alpha'.setup(require'alpha.themes.startify'.config)
-    end
+    "folke/snacks.nvim",
+    ---@type snacks.Config
+    opts = {
+      dashboard = {
+        -- your dashboard configuration comes here
+        -- or leave it empty to use the default settings
+        -- refer to the configuration section below
+
+        formats = {
+          key = function(item)
+            return {
+              { "[", hl = "special" },
+              { item.key, hl = "key" },
+              { "]", hl = "special" }
+            }
+          end,
+        },
+        sections = {
+          {
+            section = "terminal",
+            cmd = "fortune | cowsay",
+            hl = "header",
+            padding = 1,
+            indent = 8
+          },
+          { title = "MRU", padding = 1 },
+          { section = "recent_files", limit = 8, padding = 1 },
+          -- { title = "MRU ", file = vim.fn.fnamemodify(".", ":~"), padding = 1 },
+          -- { section = "recent_files", cwd = true, limit = 8, padding = 1 },
+          { title = "Sessions", padding = 1 },
+          { section = "projects", padding = 1 },
+          { title = "Bookmarks", padding = 1 },
+          { section = "keys" },
+        },
+      },
+    },
   },
   {
     "vim-airline/vim-airline",
