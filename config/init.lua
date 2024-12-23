695811b833b35c7129eadaab995b2a12ec5af209
Wei Cheng <xsnaruto@users.noreply.github.com>
2024-12-23 21:51:54 +0800

    Update init.lua

--- config/init.lua
+++ config/init.lua
@@ -118,9 +118,42 @@ require("lazy").setup({
   {
     'goolord/alpha-nvim',
     dependencies = { 'echasnovski/mini.icons' },
-    config = function ()
-        require'alpha'.setup(require'alpha.themes.startify'.config)
-    end
+    -- config = function ()
+    --     require'alpha'.setup(require'alpha.themes.startify'.config)
+    -- end
+    config = function()
+        local alpha = require("alpha")
+        local startify = require("alpha.themes.startify")
+
+        startify.section.header.val = {
+            [[             _             _               ]], 
+            [[        ___ | | _ __ ___  (_) _   _  _ __  ]],
+            [[       / _ \| || '_ ` _ \ | || | | || '__| ]],
+            [[      |  __/| || | | | | || || |_| || |    ]],
+            [[       \___||_||_| |_| |_||_| \__,_||_|    ]],
+        }
+
+        startify.opts.layout[1].val = 2
+        startify.opts.opts.margin = 43
+
+        -- disable MRU
+        startify.section.mru.val = { { type = "padding", val = 0 } }
+
+        -- Set menu
+        startify.section.top_buttons.val = {
+            startify.button("e", " > New File", "<cmd>ene<CR>"),
+            startify.button("SPC ee", " > Toggle file explorer", "<cmd>NvimTreeToggle<CR>"),
+            startify.button("SPC pff", " > Find File", "<cmd>Telescope find_files<CR>"),
+            startify.button("SPC pfw", " > Find Word", "<cmd>Telescope live_grep<CR>"),
+            startify.button("SPC wr", " > Restore Session For Current Directory", "<cmd>SessionRestore<CR>"),
+        }
+
+        -- Send config to alpha
+        alpha.setup(startify.config)
+
+        -- Disable folding on alpha buffer
+        vim.cmd([[autocmd FileType alpha setlocal nofoldenable]])
+    end,
   },
   {
     "vim-airline/vim-airline",
