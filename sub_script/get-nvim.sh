36be8d86c2edcaf051ea426f765cde4c166392ff
Wei Cheng <xsnaruto@users.noreply.github.com>
2024-12-23 17:14:42 +0800

    Update get-nvim.sh

--- sub_script/get-nvim.sh
+++ sub_script/get-nvim.sh
@@ -3,19 +3,23 @@ echo "Start install neovim"
 sleep 1s
 
 # 安装最新 NeoVim
-sudo apt install software-properties-common -y >>/dev/null 2>&1
-sudo add-apt-repository ppa:neovim-ppa/stable -y >>/dev/null 2>&1
-sudo apt update && apt install neovim -y >>/dev/null 2>&1
+# sudo apt install software-properties-common -y >>/dev/null 2>&1
+# sudo add-apt-repository ppa:neovim-ppa/stable -y >>/dev/null 2>&1
+# sudo apt update && apt install neovim -y >>/dev/null 2>&1
+sudo apt install -y snapd
+sudo snap install nvim --classic
 
 # 更新 node.js 和 npm
 sudo npm install -g n && n lts >>/dev/null 2>&1
+sudo npm install -g prettier >>/dev/null 2>&1
+sudo npm install -g @johnnymorganz/stylua-bin >>/dev/null 2>&1
 
 # Clean may exist junk file
 rm -rf ~/.config/nvim >>/dev/null 2>&1
 # rm -rf ~/.config/coc >>/dev/null 2>&1
 # Create neovim config path
-mkdir ~/.config/nvim -p >>/dev/null 2>&1
-mkdir ~/.config/nvim/plugged >>/dev/null 2>&1
+# mkdir ~/.config/nvim -p >>/dev/null 2>&1
+# mkdir ~/.config/nvim/plugged >>/dev/null 2>&1
 
 # Install neovim plugin
 # sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
