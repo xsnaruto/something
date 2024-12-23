7aa769d58489c13ac7fd725a6925d010adc32e30
Wei Cheng <xsnaruto@users.noreply.github.com>
2024-12-24 01:20:22 +0800

    Update get-nvim.sh

--- sub_script/get-nvim.sh
+++ sub_script/get-nvim.sh
@@ -3,19 +3,26 @@ echo "Start install neovim"
 sleep 1s
 
 # 安装最新 NeoVim
+echo "Update & Install DEP"
 sudo apt install software-properties-common -y >>/dev/null 2>&1
 sudo add-apt-repository ppa:neovim-ppa/stable -y >>/dev/null 2>&1
-sudo apt update && apt install neovim -y >>/dev/null 2>&1
+sudo apt update >>/dev/null 2>&1
+sudo apt install neovim -y >>/dev/null 2>&1
+sudo apt install fortune -y >>/dev/null 2>&1
+sudo apt install cowsay -y >>/dev/null 2>&1
 # sudo apt remove -y neovim >>/dev/null 2>&1
 # sudo apt install -y snapd
 # sudo snap install nvim --classic
 
 # 更新 node.js 和 npm
-sudo npm install -g n && n lts >>/dev/null 2>&1
+echo "Dealing Node DEP"
+sudo npm install -g n >>/dev/null 2>&1
+n lts >>/dev/null 2>&1
 sudo npm install -g prettier >>/dev/null 2>&1
 sudo npm install -g @johnnymorganz/stylua-bin >>/dev/null 2>&1
 
 # Clean may exist junk file
+echo "Cleaning Old Neovim data"
 rm -rf ~/.config/nvim >>/dev/null 2>&1
 # rm -rf ~/.config/coc >>/dev/null 2>&1
 # Create neovim config path
@@ -31,8 +38,9 @@ mkdir ~/.config/nvim -p >>/dev/null 2>&1
 # wget -O ~/.config/nvim/colors/molokai.vim https://raw.githubusercontent.com/tomasr/molokai/master/colors/molokai.vim >>/dev/null 2>&1
 
 # Neovim config restore
+echo "Getting Neovim Configuration"
 wget -O ~/.config/nvim/init.lua https://github.com/xsnaruto/something/raw/main/config/init.lua >>/dev/null 2>&1
 
 # Load PlugIns
-echo "finished, type <enter> continue"
+echo "Finished, type <enter> continue"
 read none
