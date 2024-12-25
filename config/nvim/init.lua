6c213e55fad27234436d2d74d46bd2dcdff278d0
xsnaruto <github@imwc.me>
2024-12-25 17:42:07 +0800

    Cancelled hide fillchars

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -31,7 +31,7 @@ vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
 
 -- 显示设置
-vim.opt.fillchars:append("eob: ")
+-- vim.opt.fillchars:append("eob: ")
 vim.opt.number = true
 vim.opt.relativenumber = false
 vim.opt.cursorline = false
