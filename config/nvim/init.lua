d8993a9e421701fb8f3bb48abc0b0f53771fd36d
xsnaruto <github@imwc.me>
2024-12-25 18:08:00 +0800

    update

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -239,6 +239,7 @@ require("lazy").setup({
   {
     "nvim-neo-tree/neo-tree.nvim",
     branch = "v3.x",
+    event = "VeryLazy",
     dependencies = {
       "nvim-lua/plenary.nvim",
       "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
