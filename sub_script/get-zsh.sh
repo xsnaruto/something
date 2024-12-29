3b56f5eaca543523e5bcfc6338179390cc242b29
xsnaruto <github@imwc.me>
2024-12-29 13:12:23 +0800

    Update

--- sub_script/get-zsh.sh
+++ sub_script/get-zsh.sh
@@ -3,28 +3,26 @@ echo "Install zsh and plugin"
 sleep 1s
 
 # Clean may exist junk file
-rm ~/.zshrc >>/dev/null 2>&1
 rm -rf ~/.oh-my-zsh >>/dev/null 2>&1
 
 # Install & Update zsh
 sudo apt install zsh -y
+
+# Install ohmyzsh
+echo "y\n" | sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" >>/dev/null 2>&1
 # Set zsh as default shell
 echo -e "/bin/zsh\n" | chsh >>/dev/null 2>&1
 
-# Install zimfw
-# echo "y\n" | sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" >>/dev/null 2>&1
-curl -fsSL https://raw.githubusercontent.com/zimfw/install/master/install.zsh | zsh >>/dev/null 2>&1
-
 # Install plugins
-# git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions >>/dev/null 2>&1
-# git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting >>/dev/null 2>&1
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions >>/dev/null 2>&1
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting >>/dev/null 2>&1
 
-# sed -i "s#plugins=(git)#plugins=(\n\tgit\n\tzsh-autosuggestions\n\tzsh-syntax-highlighting\n)#g" ~/.zshrc >>/dev/null 2>&1
+sed -i "s#plugins=(git)#plugins=(\n\tgit\n\tzsh-autosuggestions\n\tzsh-syntax-highlighting\n)#g" ~/.zshrc >>/dev/null 2>&1
 
 # Clean junk & Install  Starship
 sh -c 'rm -rf "$(command -v 'starship')"' >>/dev/null 2>&1
-# sed -i "/# Starship/d" ~/.zshrc >>/dev/null 2>&1
-# sed -i "/eval \"\$(starship init zsh)\"/d" ~/.zshrc >>/dev/null 2>&1
+sed -i "/# Starship/d" ~/.zshrc >>/dev/null 2>&1
+sed -i "/eval \"\$(starship init zsh)\"/d" ~/.zshrc >>/dev/null 2>&1
 
 curl -sS https://starship.rs/install.sh | sh
 echo "" >>~/.zshrc
