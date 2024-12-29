6feaae1bc512f0f3c70023d4604530964f426f77
xsnaruto <github@imwc.me>
2024-12-30 02:05:53 +0800

    uldate

--- sub_script/get-zim.sh
+++ sub_script/get-zim.sh
@@ -6,12 +6,6 @@ sleep 1s
 sh -c 'rm -rf "$(command -v 'starship')"' >>/dev/null 2>&1
 curl -sS https://starship.rs/install.sh | sh
 
-# Clean may exist junk file
-rm ~/.zshrc >>/dev/null 2>&1
-rm ~/.zimrc >>/dev/null 2>&1
-rm -rf ~/.oh-my-zsh >>/dev/null 2>&1
-rm -rf ~/.zim >>/dev/null 2>&1
-
 # Install/Update zsh
 sudo apt install zsh -y
 # Set zsh as default shell
@@ -21,7 +15,14 @@ echo -e "/bin/zsh\n" | chsh >>/dev/null 2>&1
 # echo "y\n" | sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" >>/dev/null 2>&1
 curl -fsSL https://raw.githubusercontent.com/zimfw/install/master/install.zsh | zsh >>/dev/null 2>&1
 
-# Install zimfw
+# Clean old config data
+rm ~/.zshrc >>/dev/null 2>&1
+rm -rf ~/.oh-my-zsh >>/dev/null 2>&1
+
+rm ~/.zimrc >>/dev/null 2>&1
+rm -rf ~/.zim >>/dev/null 2>&1
+
+# Restore zim config
 wget -o .zimrc https://raw.githubusercontent.com/xsnaruto/something/refs/heads/main/config/zimfw/.zimrc >>/dev/null 2>&1
 
 echo "finished, type <enter> continue"
